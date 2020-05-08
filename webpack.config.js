49399505940e032c9c955c31421cb628fe962ad7
AN <andre_nuechter@hotmail.de>
2020-05-08 18:01:46 +0200

    Some minor changes to tooling

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
         path.join(__dirname, '/src/js/card-slots.js')
     ],
     output: {
-        filename: 'index.js',
-        path: path.join(__dirname, '/docs')
+        filename: 'index.js'
     }
 };
