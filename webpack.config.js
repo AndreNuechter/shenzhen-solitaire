87a855149bd80bdc1d0d5ef314d9da6493a68001
AN <andre_nuechter@hotmail.de>
2020-08-15 20:21:39 +0200

    Exclude service-worker from bundle and npm audit fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: [
-        path.join(__dirname, '/src/js/index.js'),
-        path.join(__dirname, '/src/js/card-slots.js')
-    ],
+    entry: {
+        index: [
+            path.join(__dirname, '/src/js/index.js'),
+            path.join(__dirname, '/src/js/card-slots.js')
+        ],
+        'service-worker': path.join(__dirname, '/src/service-worker.js')
+    },
     output: {
-        filename: 'index.js'
+        filename: '[name].js'
     }
 };
