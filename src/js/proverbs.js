53b205b477ca7c1fd3fdb586278fdd6bfb7cf40f
Andre Nuechter <andre_nuechter@hotmail.de>
2025-01-05 13:54:06 +0100

    Add some gambling quotes

--- src/js/proverbs.js
+++ src/js/proverbs.js
@@ -1,8 +1,16 @@
 export default [
     'At the gambling table, there are no fathers and sons.',
     'Reform a gambler. Cure leprosy.',
-    'A pack of cards is the devil’s prayer-book.',
+    'A pack of cards is the devil\'s prayer-book.',
     'Rich gamblers and old trumpeters are rare.',
     'Luck never gives; it only lends.',
     'Before you gamble: Know the rules, the stakes and predetermine the quitting time.',
+    'Gambling: The sure way of getting nothing for something.',
+    'The highest form of wisdom is to know when to play and when to stay away.',
+    'If you can\'t spot the sucker in the first half hour at the table, then you are the sucker.',
+    'Gambling is not about money; it is about the thrill of the risk.',
+    'Hedging is for gardeners.',
+    'You can\'t win if you don\'t play',
+    'Luck is probability taken personally',
+    'Quit while you\'re ahead. All the best gamblers do.',
 ];
